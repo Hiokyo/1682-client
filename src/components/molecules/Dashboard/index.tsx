e79b13defb8321502301ca8c1ae60088c5ff1faf
trung-hieu <hieutran235@gmail.com>
2023-04-14 10:01:53 +0700

    feat/modal-chat

--- src/components/molecules/Dashboard/index.tsx
+++ src/components/molecules/Dashboard/index.tsx
@@ -1,54 +1,364 @@
-import React from 'react';
-import { Column, Line } from '@ant-design/plots';
-import styles from './styles.module.scss';
-import { Row, Col } from 'antd';
+import React, { useMemo } from "react";
+import { Column, Bar, Pie, DualAxes } from "@ant-design/plots";
+import { Row, Col, Card, Statistic } from "antd";
+import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
+// import { useDashboard } from "~/hooks/useDashboard";
+
+import Meta from "antd/es/card/Meta";
+import Spin from "~/components/atoms/Spin";
+import styles from "./styles.module.scss";
 
 const Dashboards = () => {
-  const data = [
-    { year: '1991', value: 3 },
-    { year: '1992', value: 4 },
-    { year: '1993', value: 3.5 },
-    { year: '1994', value: 5 },
-    { year: '1995', value: 4.9 },
-    { year: '1996', value: 6 },
-    { year: '1997', value: 7 },
-    { year: '1998', value: 9 },
-    { year: '1999', value: 13 },
-  ];
+  const today = new Date();
+  const currentYear = today.getFullYear();
+  const lastYear = currentYear - 1;
+  // const { data, isFetching, isLoading } = useDashboard(true);
+  const dataDashBoard: any = [];
+
+  // number user contributors by month
+  const dataColumnChart = useMemo(() => {
+    if (dataDashBoard) {
+      return [
+        {
+          month: "Jan",
+          value: dataDashBoard.ideaCountByDepartment?.Jan?.userCount,
+        },
+        {
+          month: "Feb",
+          value: dataDashBoard.ideaCountByDepartment?.Feb?.userCount,
+        },
+        {
+          month: "Mar",
+          value: dataDashBoard.ideaCountByDepartment?.Mar?.userCount,
+        },
+        {
+          month: "Apr",
+          value: dataDashBoard.ideaCountByDepartment?.Apr?.userCount,
+        },
+        {
+          month: "May",
+          value: dataDashBoard.ideaCountByDepartment?.May?.userCount,
+        },
+        {
+          month: "Jun",
+          value: dataDashBoard.ideaCountByDepartment?.Jun?.userCount,
+        },
+        {
+          month: "Jul",
+          value: dataDashBoard.ideaCountByDepartment?.Jul?.userCount,
+        },
+        {
+          month: "Aug",
+          value: dataDashBoard.ideaCountByDepartment?.Aug?.userCount,
+        },
+        {
+          month: "Sep",
+          value: dataDashBoard.ideaCountByDepartment?.Sep?.userCount,
+        },
+        {
+          month: "Oct",
+          value: dataDashBoard.ideaCountByDepartment?.Oct?.userCount,
+        },
+        {
+          month: "Nov",
+          value: dataDashBoard.ideaCountByDepartment?.Nov?.userCount,
+        },
+        {
+          month: "Dec",
+          value: dataDashBoard.ideaCountByDepartment?.Dec?.userCount,
+        },
+      ];
+    }
+  }, [dataDashBoard?.ideaCountByDepartment]);
 
   const config = {
-    data,
-    xField: 'year',
-    yField: 'value',
+    data: dataColumnChart ? dataColumnChart : [],
+    xField: "month",
+    yField: "value",
   };
-  return (
-    <Row 
-      className={styles.dashboardContainer} 
-      gutter={[ 16, 16]}
-      justify="space-around" align="middle"
-    >
-      <Col className={styles.chart} span={11}>
-        <Line  {...config} />
-      </Col>
-      <Col className={styles.chart} span={11}>
-        <Column  {...config}/>
-      </Col>
-      <Col  className={styles.chart} span={11}>
-        <Line {...config} />
-      </Col>
-      <Col className={styles.chart} span={11}>
-        <Column  {...config}/>
-      </Col>
-    </Row>
-    // <div className={styles.dashboardContainer}>
-    //   <div className={styles.chart}>
-    //     <Line {...config} />
-    //   </div>
-    //   <div className={styles.chart}>
-    //     <Column {...config} />
-    //   </div>
-    // </div>
-  )
-}
 
-export default Dashboards
+  // total ideas for all system in each year
+  const dataPieChart = useMemo(() => {
+    if (dataDashBoard) {
+      return dataDashBoard?.ideasByYear?.map((item: any) => ({
+        type: item.year,
+        value: item.ideasCount,
+      }));
+    }
+  }, [dataDashBoard?.ideaCountByDepartment]);
+
+  const configPieChart = {
+    data: dataPieChart ? dataPieChart : [],
+    appendPadding: 10,
+    angleField: "value",
+    colorField: "type",
+    radius: 0.9,
+    label: {
+      type: "inner",
+      offset: "-30%",
+      content: ({ percent }: any) => `${(percent * 100)?.toFixed(0)}%`,
+      style: {
+        fontSize: 14,
+        textAlign: "center",
+      },
+    },
+    interactions: [
+      {
+        type: "element-active",
+      },
+    ],
+  };
+  // total ideas for each departments
+  const dataLineChart = useMemo(() => {
+    if (dataDashBoard) {
+      const dataInteraction = dataDashBoard.departmentInteractionCount;
+      if (dataInteraction) {
+        const dataChart = dataInteraction.map((item: any) => ({
+          department: item.name,
+          like: item.likeCount,
+          view: item.viewCount,
+        }));
+        return dataChart;
+      }
+    }
+  }, [dataDashBoard?.departmentInteractionCount]);
+
+  const configLineChart = {
+    data: dataLineChart ? [dataLineChart, dataLineChart] : [{}, {}],
+    xField: "department",
+    yField: ["like", "view"],
+    geometryOptions: [
+      {
+        geometry: "line",
+        color: "#5B8FF9",
+      },
+      {
+        geometry: "line",
+        color: "#5AD8A6",
+      },
+    ],
+  };
+
+  const dataBarChart = useMemo(() => {
+    if (dataDashBoard) {
+      return [
+        {
+          month: "Jan",
+          ideaCount:
+            dataDashBoard.ideaCountByDepartment?.Jan?.departments[0]?.ideaCount,
+        },
+        {
+          month: "Feb",
+          ideaCount:
+            dataDashBoard.ideaCountByDepartment?.Feb?.departments[0]?.ideaCount,
+        },
+        {
+          month: "Mar",
+          ideaCount:
+            dataDashBoard.ideaCountByDepartment?.Mar?.departments[0]?.ideaCount,
+        },
+        {
+          month: "Apr",
+          ideaCount:
+            dataDashBoard.ideaCountByDepartment?.Apr?.departments[0]?.ideaCount,
+        },
+        {
+          month: "May",
+          ideaCount:
+            dataDashBoard.ideaCountByDepartment?.May?.departments[0]?.ideaCount,
+        },
+        {
+          month: "Jun",
+          ideaCount:
+            dataDashBoard.ideaCountByDepartment?.Jun?.departments[0]?.ideaCount,
+        },
+      ];
+    }
+  }, [dataDashBoard?.ideaCountByDepartment]);
+
+  const configBarChart = {
+    data: dataBarChart ? dataBarChart : [],
+    xField: "ideaCount",
+    yField: "month",
+  };
+
+  const info = useMemo(() => {
+    if(dataDashBoard) {
+      const { todayIdeaCount, yesterdayIdeaCount } = dataDashBoard;
+  
+      let percent = 0;
+      let type = "equal";
+  
+      if (todayIdeaCount > yesterdayIdeaCount) {
+        type = "greaterThan";
+        percent =
+          yesterdayIdeaCount === 0 ? 100 : todayIdeaCount / yesterdayIdeaCount;
+      } else if (todayIdeaCount < yesterdayIdeaCount) {
+        type = "lessThan";
+        percent = todayIdeaCount === 0 ? 100 : yesterdayIdeaCount / todayIdeaCount;
+      }
+  
+      return {
+        type,
+        percent,
+      };
+    }
+  }, [dataDashBoard]);
+
+  const infoByYear = useMemo(() => {
+    if(dataDashBoard) {
+      let percent = 0;
+      let type = "equal";
+      const dataNow = (dataDashBoard?.ideasByYear)?.find((item: any) => (item.year === currentYear))?.ideasCount;
+      const dataPast = (dataDashBoard?.ideasByYear)?.find((item: any) => (item.year === lastYear))?.ideasCount;
+      if (dataNow > dataPast) {
+        type = "greaterThan";
+        percent = dataPast === 0 ? 100 : dataNow / dataPast;
+      } else if (dataNow < dataPast) {
+        type = "lessThan";
+        percent = dataNow === 0 ? 100 : dataPast / dataNow;
+      }
+      return {
+        type,
+        percent,
+      };
+    }
+  },[dataDashBoard])
+  return (
+    <div className={styles.dashboardContainer}>
+      {/* <Spin spinning={isLoading || isFetching}> */}
+        <Row gutter={25}>
+          <Col 
+            xxl={{ span: 12}}
+            xl={{ span: 12}}
+            lg={{ span: 12}}
+            md={{ span: 24}}
+            sm={{ span: 24}}
+            xs={{ span: 24}}
+          >
+            <Card>
+              <h3>Today's ideas</h3>
+              <div className={styles.statistic}>
+                <h1 className={styles.cardInfoValue}>
+                  {dataDashBoard?.todayIdeaCount}
+                </h1>
+                <Statistic
+                  className="mt-2 ml-2"
+                  value={
+                    info?.percent
+                  }
+                  precision={2}
+                  valueStyle={{ color:  info?.type === 'lessThan' ? 'red' : "#3f8600" , fontSize: 14 }}
+                  prefix={ info?.type === 'lessThan' ? <ArrowDownOutlined/> : <ArrowUpOutlined />}
+                  suffix="%"
+                />
+              </div>
+            </Card>
+          </Col>
+          <Col 
+            xxl={{ span: 12}}
+            xl={{ span: 12}}
+            lg={{ span: 12}}
+            md={{ span: 24}}
+            sm={{ span: 24}}
+            xs={{ span: 24}}
+          >
+            <Card>
+              <h3>Year's ideas</h3>
+              <div className={styles.statistic}>
+                <h1 className={styles.cardInfoValue}>
+                  {(dataDashBoard?.ideasByYear)?.find((item: any) => (item.year === currentYear))?.ideasCount}
+                </h1>
+                <Statistic
+                  className="mt-2 ml-2"
+                  value={infoByYear?.percent}
+                  precision={2}
+                  valueStyle={{ color:  infoByYear?.type === 'lessThan' ? 'red' : "#3f8600" , fontSize: 14 }}
+                  prefix={ infoByYear?.type === 'lessThan' ? <ArrowDownOutlined/> : <ArrowUpOutlined />}
+                  suffix="%"
+                />
+              </div>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="mt-4" gutter={16}>
+          <Col 
+            xxl={{ span: 10}}
+            xl={{ span: 10}}
+            lg={{ span: 10}}
+            md={{ span: 24}}
+            sm={{ span: 24}}
+            xs={{ span: 24}}
+          >
+            <Card>
+              <Card className={styles.cardColumnChart}>
+                <Column {...config} height={300} color="#dfe7f7" />
+              </Card>
+              <Meta
+                style={{ marginTop: 23 }}
+                title="user contribution"
+                description={
+                  <Statistic
+                    title="than last month"
+                    value={5.63}
+                    style={{ display: "flex" }}
+                    precision={2}
+                    valueStyle={{
+                      color: "#3f8600",
+                      fontSize: 14,
+                      marginLeft: 5,
+                    }}
+                    prefix={<ArrowUpOutlined />}
+                    suffix="%"
+                  />
+                }
+              />
+            </Card>
+          </Col>
+          <Col 
+            xxl={{ span: 14}}
+            xl={{ span: 14}}
+            lg={{ span: 14}}
+            md={{ span: 24}}
+            sm={{ span: 24}}
+            xs={{ span: 24}}
+          >
+            <Card
+              className={styles.dualLineChart}
+            >
+              <DualAxes {...configLineChart} height={300} />
+            </Card>
+          </Col>
+        </Row>
+        <Row className="mt-4" gutter={[16, 16]}>
+          <Col 
+            xxl={{ span: 12}}
+            xl={{ span: 12}}
+            lg={{ span: 12}}
+            md={{ span: 24}}
+            sm={{ span: 24}}
+            xs={{ span: 24}}
+          >
+            <Card>
+              <Pie {...configPieChart} height={300} />
+            </Card>
+          </Col>
+          <Col 
+            xxl={{ span: 12}}
+            xl={{ span: 12}}
+            lg={{ span: 12}}
+            md={{ span: 24}}
+            sm={{ span: 24}}
+            xs={{ span: 24}}
+          >
+            <Card>
+              <Bar {...configBarChart} height={300} />
+            </Card>
+          </Col>
+        </Row>
+      {/* </Spin> */}
+    </div>
+  );
+};
+
+export default Dashboards;
--- src/components/molecules/FriendList/index.tsx
+++ src/components/molecules/FriendList/index.tsx
@@ -1,5 +1,5 @@
-import { List, Avatar } from 'antd';
-import React from 'react'
+import { List, Avatar, Drawer } from 'antd';
+import React, { useState } from 'react'
 import styles from './styles.module.scss'
 import { PARAMS_GET_ALL } from '~/utils/constant';
 import { useFriends } from '~/hooks/useFriends';
@@ -9,6 +9,7 @@ import Spin from '~/components/atoms/Spin';
 const FriendList = () => {
   const userData = useAppSelector((state) => state.userInfo.userData);
 
+  const [open, setOpen] = useState(false);
   const {data, isLoading, isFetching} = useFriends({
     page: 1,
     limit: 999,
@@ -16,15 +17,28 @@ const FriendList = () => {
   })
 
   const friends = data?.data?.following;
+  const onClose = () => {
+    setOpen(false);
+  };
+
+  const messages = [
+    { id: 1, user: {id: 1, name: 'Quan'}, content: 'Hello' },
+    { id: 2, user: {id: 2, name: 'Hieu'}, content: 'How are you?' },
+    { id: 3, user: {id: 1, name: 'Quan'}, content: 'I am fine' },
+  ];
   return (
     <Spin spinning={isLoading || isFetching}>
       <List
         className={styles.listContainer}
         dataSource={friends}
         renderItem={(item: any) => (
-          <List.Item key={item.user._id}>
+          <List.Item 
+            key={item.user._id}
+            className={styles.friendItem}
+            onClick={() => setOpen(true)}
+          >
             <List.Item.Meta
-              avatar={<Avatar size={36} src={item.user.avatar} />}
+              avatar={<Avatar size={40} src={item.user.avatar} />}
               title={
                 <div>
                   {item.user.firstName} {item.user.lastName}
@@ -35,6 +49,35 @@ const FriendList = () => {
           </List.Item>
         )}
       />
+      <Drawer
+        title='Chat'
+        placement="right"
+        onClose={onClose}
+        className={styles.modalChat}
+        open={open}
+        // style={{width: '200px'}}
+        // extra={
+        //   <Space>
+        //     <Button onClick={onClose}>Cancel</Button>
+        //     <Button type="primary" onClick={onClose}>
+        //       OK
+        //     </Button>
+        //   </Space>
+        // }
+      >
+        <List
+          dataSource={messages}
+          className={styles.listChat}
+          renderItem={(item) => (
+            <List.Item
+              className={item.user.id === 1 ? styles.sender : styles.receiver}
+              key={item.id}
+            >
+              {item.content}
+            </List.Item>
+          )}
+        />
+      </Drawer>
     </Spin>
   );
 }
